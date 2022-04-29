e75d5d48ed1b79a70d553332607fc3b04656f225
khalid umar <dreamiwoarld.online@gmail.com>
2022-04-29 16:36:14 +0600

    init new app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-purple-500'> hi this is initialize project</h1>
+      <h1 className='text-purple-300'>this is home page</h1>
     </div>
   );
 }
