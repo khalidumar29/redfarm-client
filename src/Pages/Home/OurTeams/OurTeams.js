44ac8cce4c2a4770ec3cc33b71bc171e4f12af40
khalid umar <dreamiwoarld.online@gmail.com>
2022-05-08 02:42:09 +0600

    finish assignment

--- src/Pages/Home/OurTeams/OurTeams.js
+++ src/Pages/Home/OurTeams/OurTeams.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Team from "./Team/Team";
 import team1 from "../../../images/team/team1.jpg";
+import team2 from "../../../images/team/images.jpg";
+import team3 from "../../../images/team/CEO.jpg";
 const OurTeam = () => {
   return (
     <div className='container'>
@@ -8,9 +10,25 @@ const OurTeam = () => {
         Our Team
       </h1>
       <div className='d-lg-flex py-lg-3 justify-content-lg-between'>
-        <Team pic={team1}></Team>
-        <Team pic={team1}></Team>
-        <Team pic={team1}></Team>
+        <Team
+          des={"Oure honarable ceo. who create this company."}
+          name={"Mark Docky"}
+          pic={team1}
+        ></Team>
+        <Team
+          des={
+            "Our honorable markating manager. who made this compnay so profitable"
+          }
+          name={"Steave Gain"}
+          pic={team2}
+        ></Team>
+        <Team
+          des={
+            "Our Honor able production enginner. who create this website so better."
+          }
+          name={"Dan Ferry"}
+          pic={team3}
+        ></Team>
       </div>
     </div>
   );
